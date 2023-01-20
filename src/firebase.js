f0ac96f5e79ffa029d5802861296c652992ce1f2
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-20 21:25:48 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhp5durVAb425H7NTyA9LVZhFOu5ESkeI',
+  authDomain: 'reactjs-netflix-5b06d.firebaseapp.com',
+  projectId: 'reactjs-netflix-5b06d',
+  storageBucket: 'reactjs-netflix-5b06d.appspot.com',
+  messagingSenderId: '622451040384',
+  appId: '1:622451040384:web:c3c17b9bb60a3f2e9d4665',
+};
