51ea5ada808fda57cafe73b4fd3c4b770e069b8b
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-21 21:43:04 +0200

    firebase config

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
 import './signup.css';
 
 const SignUp = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <div className="signup">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <input
+          ref={emailRef}
+          type="email"
+          placeholder="Email Address"
+        />
+        <input
+          ref={passwordRef}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
 
         <h4>
           <span className="signup__gray">New to netflix?</span>{' '}
-          <span className="signup__link">Sign Up now</span>
+          <span className="signup__link" onClick={register}>
+            Sign Up now
+          </span>
         </h4>
       </form>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAhp5durVAb425H7NTyA9LVZhFOu5ESkeI',
   authDomain: 'reactjs-netflix-5b06d.firebaseapp.com',
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: '622451040384',
   appId: '1:622451040384:web:c3c17b9bb60a3f2e9d4665',
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const auth = getAuth();
+
+export default db;
