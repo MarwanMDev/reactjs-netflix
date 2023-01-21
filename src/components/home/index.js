731aa2511c8a64916aa8cd25e68ed31c40e66254
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-21 10:07:23 +0200

    hero section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Hero from '../hero';
 import Navbar from '../navbar';
 import './home.css';
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+
+      <Hero />
     </div>
   );
 };
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.css';
 import LOGO from '../../assets/Netflix-logo-red-black-png.png';
 import DEFAULTUSER from '../../assets/default-user.png';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const navbarTransition = () => {
+    if (window.scrollY > 100) setShow(true);
+    else setShow(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', navbarTransition);
+    return () =>
+      window.removeEventListener('scroll', navbarTransition);
+  }, []);
+
   return (
-    <div className="navbar navbar__black">
+    <div className={`navbar ${show && 'navbar__black'}`}>
       <div className="navbar__contents">
         <img src={LOGO} alt="logo" className="navbar__logo" />
         <img
